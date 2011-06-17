b8cd01482f5b88d348ad2592c07bbb9e73d0434c
rockflyrain@gmail.com <rockflyrain@gmail.com>
2011-06-17 15:28:38 +0800

    二分查找法

---
+++ groovyDemo/src/main/java/deppDemo/Man.groovy
@@ -0,0 +1,16 @@
+package deppDemo
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 11-6-17
+ * Time: 上午10:41
+ * To change this template use File | Settings | File Templates.
+ */
+public class Man {
+    def name="Moon"
+    def introduce()
+    {
+        return "I'm $name"
+    }
+}
---
+++ groovyDemo/src/main/java/deppDemo/TestGroovyEquals.groovy
@@ -0,0 +1,9 @@
+package deppDemo
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Admin
+ * Date: 11-6-17
+ * Time: 上午10:59
+ * To change this template use File | Settings | File Templates.
+ */
