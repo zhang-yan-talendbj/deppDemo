8e1e0db47e9fb26667e161ad7785256faa91a043
yan-zhang <rockflyrain@gmail.com>
2011-09-27 14:34:32 +0800

    create archetype script

---
+++ web.bat
@@ -0,0 +1,3 @@
+call mvn archetype:generate -B -DarchetypeGroupId=com.caribe.stone -DarchetypeArtifactId=com.caribe.stone.archetype.web-archetype -DarchetypeVersion=0.0.1-SNAPSHOT -DgroupId=com.caribe.stone -DartifactId=com.caribe.stone.%1 -Dversion=1.0 
+pause
+exit
