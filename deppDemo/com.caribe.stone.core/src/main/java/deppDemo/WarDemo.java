1c994626abba965bcb8ffcb2c7c76ca56f1e1ce7
rockflyrain@gmail.com <rockflyrain@gmail.com@3ad29557-59fa-f66c-413b-6ab5be73a090>
2011-11-09 12:37:13 +0000

    git-svn-id: https://caribe.googlecode.com/svn/trunk@46 3ad29557-59fa-f66c-413b-6ab5be73a090

--- deppDemo/com.caribe.stone.core/src/main/java/deppDemo/WarDemo.java
+++ deppDemo/com.caribe.stone.core/src/main/java/deppDemo/WarDemo.java
@@ -1,17 +1,36 @@
 package deppDemo;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 /**
  * Hello world!
- *
+ * 
  */
-public class WarDemo
-{
-    public static void main( String[] args ) {
-        ArrayList list=new ArrayList();
-        list.add(0,"aaa");
-        System.out.println(list.get(0));
+public class WarDemo {
+	public static void main(String[] args) {
+		try {
+			Process p = Runtime.getRuntime().exec("cmd /c time");
+			// p.waitFor();
+			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 
-    }
+			if (!br.ready()) {
+				Thread.sleep(100);
+			}
+			while (true) {
+				String s = br.readLine();
+				if (s == null)
+					break;
+				System.out.println(s);
+			}
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
