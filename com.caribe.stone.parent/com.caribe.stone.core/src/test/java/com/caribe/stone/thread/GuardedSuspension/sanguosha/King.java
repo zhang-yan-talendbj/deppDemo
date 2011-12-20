2156df775327c897324a15d9f1109f52051b9fd5
yan-zhang <rockflyrain@gmail.com>
2011-12-20 17:35:34 +0800

    add guardedSuspension pattern

---
+++ com.caribe.stone.parent/com.caribe.stone.core/src/test/java/com/caribe/stone/thread/GuardedSuspension/sanguosha/King.java
@@ -0,0 +1,49 @@
+package com.caribe.stone.thread.GuardedSuspension.sanguosha;
+
+public class King {
+	private int hp;
+	private String name;
+	private String lastWords;
+	private int kill;
+	private int save;
+
+	public King(int hp, String name, String lastWords) {
+		this.hp = hp;
+		this.name = name;
+		this.lastWords = lastWords;
+	}
+
+	@Override
+	public String toString() {
+		return "King [hp=" + hp + ", name=" + name + "]";
+	}
+
+	public synchronized int save() {
+		save++;
+		while (hp >= 500) {
+			try {
+				wait();
+			} catch (InterruptedException e) {
+			}
+		}
+		hp++;
+		return hp;
+	}
+
+	public synchronized int kill() {
+		kill++;
+		if (hp <= 0) {
+			System.out.println(lastWords+hp);
+			System.out.println("Kill:"+kill+":"+"Save:"+save);
+			System.exit(0);
+		} else {
+			hp--;
+			notifyAll();
+		}
+		return hp;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
--- com.caribe.stone.parent/com.caribe.stone.selenium/src/test/java/Demo.java
+++ com.caribe.stone.parent/com.caribe.stone.selenium/src/test/java/Demo.java
@@ -1,5 +1,8 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import com.thoughtworks.selenium.DefaultSelenium;
@@ -8,7 +11,11 @@ public class Demo {
 
 	public static void main(String[] args) {
 		WebDriver wd = new InternetExplorerDriver();
-		wd.get("http://www.google.com.hk/");
-		System.out.println(wd.findElement(By.xpath("//input[@name='btnI']")).getTagName());
+		wd.get("http://cibwkdp000105:9086/NotificationWeb/search.faces");
+		List<WebElement> findElements = wd.findElements(By.xpath("//input"));
+		for (WebElement e : findElements) {
+			System.out.println(e.getAttribute("value"));
+		}
+		wd.quit();
 	}
 }
