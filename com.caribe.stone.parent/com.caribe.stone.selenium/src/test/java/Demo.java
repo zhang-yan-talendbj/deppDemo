1a5c59e34be8eafdf1602719690e501a89f09a6a
thinkdeeply <rockflyrain@gmail.com>
2011-12-21 20:50:44 +0800

    U

--- com.caribe.stone.parent/com.caribe.stone.selenium/src/test/java/Demo.java
+++ com.caribe.stone.parent/com.caribe.stone.selenium/src/test/java/Demo.java
@@ -1,14 +1,24 @@
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-
-import com.thoughtworks.selenium.DefaultSelenium;
 
 public class Demo {
 
 	public static void main(String[] args) {
-		WebDriver wd = new InternetExplorerDriver();
-		wd.get("http://www.google.com.hk/");
-		System.out.println(wd.findElement(By.xpath("//input[@name='btnI']")).getTagName());
+		WebDriver wd;
+		wd = new InternetExplorerDriver();
+		// wd=new FirefoxDriver();
+		// wd=new HtmlUnitDriver();
+		wd.get("http://www-3a5e5c4f59a:9080/WSsamples/en/index.html");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(wd.findElement(By.xpath("//head")));
 	}
 }
