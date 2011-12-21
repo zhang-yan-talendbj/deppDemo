3d9aafdff10e1008f7930426f806e05a23871a8e
yan-zhang <rockflyrain@gmail.com>
2011-12-21 18:07:30 +0800

    add balk pattern

--- com.caribe.stone.parent/com.caribe.stone.core/src/main/java/com/caribe/stone/jsoup/Demo.java
+++ com.caribe.stone.parent/com.caribe.stone.core/src/main/java/com/caribe/stone/jsoup/Demo.java
@@ -1,6 +1,7 @@
 package com.caribe.stone.jsoup;
 
 import java.io.UnsupportedEncodingException;
+import java.util.Calendar;
 
 public class Demo {
 
@@ -9,5 +10,11 @@ public class Demo {
 		String x = "http://localhost:8091/wiki/%E9%A1%B9%E7%9B%AE%E7%AE%A1%E7%90%86";
 		String str = java.net.URLDecoder.decode(x,"utf-8");
 		System.out.println(str);
+		
+	}
+	private String content;
+	public void name() {
+		Calendar.getInstance();
+		
 	}
 }
