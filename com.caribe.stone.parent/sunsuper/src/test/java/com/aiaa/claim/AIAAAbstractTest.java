6e5d1018471191f1d999ed306c351ac5b0288bb4
yan-zhang <rockflyrain@gmail.com>
2011-12-27 18:09:17 +0800

    U

---
+++ com.caribe.stone.parent/sunsuper/src/test/java/com/aiaa/claim/AIAAAbstractTest.java
@@ -0,0 +1,65 @@
+package com.aiaa.claim;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+public abstract class AIAAAbstractTest {
+	protected static WebDriver wd;
+
+	@AfterClass
+	public static void destoryDriver() {
+		// wd.quit();
+	}
+
+	@Before
+	public void setUp() {
+		if (wd == null) {
+			DriverType type = getTyep();
+			switch (type) {
+			case ie:
+				wd = new InternetExplorerDriver();
+				break;
+			case firefox:
+				wd = new FirefoxDriver();
+				break;
+			case html:
+				wd = new HtmlUnitDriver(true);
+			default:
+				wd = new HtmlUnitDriver(true);
+				break;
+			}
+			wd.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
+		}
+	}
+
+	protected DriverType getTyep() {
+		return DriverType.ie;
+	}
+
+	protected void setValueByName(String value, String name) {
+		wd.findElement(By.name(name)).sendKeys(value);
+	}
+
+	protected void setValueById(String value, String id) {
+		wd.findElement(By.id(id)).sendKeys(value);
+	}
+
+	protected void clickByName(String name) {
+		wd.findElement(By.name(name)).click();
+	}
+
+	protected void clickById(String id) {
+		wd.findElement(By.id(id)).click();
+	}
+}
+
+enum DriverType {
+	ie, firefox, html
+}
---
+++ com.caribe.stone.parent/sunsuper/src/test/java/com/aiaa/claim/AssessClaimTest.java
@@ -0,0 +1,78 @@
+package com.aiaa.claim;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class AssessClaimTest extends AIAAAbstractTest {
+	@Test
+	public void testName() throws Exception {
+		wd.findElement(By.name("inputPolicyNumber")).sendKeys("MP9883");
+		wd.findElement(By.name("inputMemberNumber")).sendKeys("4");
+		SeleniumUtils.select(wd.findElement(By.name("selectPolicySource")),"Compass");
+		
+		wd.findElement(By.id("recordClaim")).click();
+		
+		recordClaim();
+	}
+	
+	private void recordClaim() {
+		SeleniumUtils.select(wd.findElement(By.name("selectBenefitName")),"DEATH COVER : GROUP LIFE DEATH SUPER");
+		setValueByName("Exclusions Test", "inputExclusions");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$4")),"Swiss Reinsurance");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$6")),"Agreed Value");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$7")),"Yes");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$9")),"Lump Sum");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$8")),"Yes");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$13")),"AAL");
+		
+		wd.findElement(By.name("next")).click();
+		
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded")),"Mr");
+		setValueById("1111-11", "number");
+		clickByName("findOccupationNumber");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$15")),"Self Employed");
+		SeleniumUtils.select(wd.findElement(By.name("aiaEmbedded$20")),"VIC");
+		
+		clickByName("checkInsuredPosIsResCheckbox");
+		clickByName("next");
+		clickByName("next");
+		clickByName("next");
+		clickByName("next");
+		clickByName("next");
+		setValueByName("inputCauseDescription test", "inputCauseDescription");
+		clickByName("next");
+		
+		clickByName("confirmTask");
+		clickByName("complete");
+		String text = wd.findElement(By.className("info")).getText();
+		System.out.println(text);
+		Pattern p=Pattern.compile("\\(.*\\)");
+		Matcher matcher = p.matcher(text);
+		if(matcher.find()){
+		String claimNumber = matcher.group();
+		claimNumber=claimNumber.substring(1, claimNumber.length()-1);
+		
+		setValueByName(claimNumber, "inputClaimNumber");
+		clickByName("search");
+		
+		wd.findElement(By.linkText(claimNumber)).click();
+		
+		clickById("AssessmentTabLink");
+		
+		clickById("assessAction");
+		}
+	}
+
+	@Before
+	public void setUp(){
+		super.setUp();
+		wd.get("http://CIBD3R8CGMD05:9080/ClaimsAdminWeb/login.jsp");
+		wd.findElement(By.id("name")).sendKeys("auth1");
+		wd.findElement(By.id("pswd")).sendKeys("123");
+		wd.findElement(By.name("action")).click();
+	}
+}
