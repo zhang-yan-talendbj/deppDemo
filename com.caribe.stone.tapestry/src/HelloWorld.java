46e8c153db0ad4f58e9b371414878ef69987d992
thinkdeeply <rockflyrain@gmail.com>
2012-01-04 00:50:27 +0800

    u

---
+++ com.caribe.stone.tapestry/src/HelloWorld.java
@@ -0,0 +1,25 @@
+
+
+import org.apache.tapestry.html.BasePage;
+
+public class HelloWorld extends BasePage {
+	private String name;
+
+	private String msg;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMsg() {
+		return msg;
+	}
+
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+}
