bf0666b9eab797072461aa7a24476440ff6326ad
yan-zhang <rockflyrain@gmail.com>
2012-01-04 16:43:14 +0800

    add tapestry demo. t3 and t5

---
+++ com.caribe.stone.parent/com.caribe.stone.tapestry3/src/main/java/com/caribe/stone/tapestry/Home.java
@@ -0,0 +1,11 @@
+package com.caribe.stone.tapestry;
+
+import org.apache.tapestry.html.BasePage;
+
+public class Home extends BasePage {
+	private String userName = "bruce";
+
+	public String getUserName() {
+		return this.userName;
+	}
+}
