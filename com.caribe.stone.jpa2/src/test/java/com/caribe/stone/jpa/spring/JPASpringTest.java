f4056ee386dcb190121a37dc1b281e6647307840
yan-zhang <rockflyrain@gmail.com>
2012-01-18 17:50:09 +0800

    update spring config to run jpa without VM arguments

--- com.caribe.stone.jpa2/src/test/java/com/caribe/stone/jpa/spring/JPASpringTest.java
+++ com.caribe.stone.jpa2/src/test/java/com/caribe/stone/jpa/spring/JPASpringTest.java
@@ -1,7 +1,5 @@
 package com.caribe.stone.jpa.spring;
-
-import static org.junit.Assert.*;
-
+import static junit.framework.Assert.*;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 
@@ -10,23 +8,22 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 import com.caribe.stone.jpa.entities.SimpleEntity;
-@ContextConfiguration(locations={"classpath:/JPA-CONFIG/spring/context.xml"})
+
+@ContextConfiguration(locations = { "classpath:/JPA-CONFIG/spring/context.xml" })
 public class JPASpringTest extends AbstractJUnit4SpringContextTests {
 
 	@Test
 	public void testName() throws Exception {
-		System.out.println(applicationContext);
-		EntityManagerFactory emf = (EntityManagerFactory) applicationContext.getBean("entityManagerFactory");
-		
+		 EntityManagerFactory emf = (EntityManagerFactory)
+		 applicationContext.getBean("entityManagerFactory");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		SimpleEntity simple = new SimpleEntity();
-		simple.setName("simple");
-		em.persist(simple);
-		em.getTransaction().commit();
-
-		assertNotNull(simple.getId());
-		SimpleEntity s = em.find(SimpleEntity.class, simple.getId());
-		assertEquals("simple", s.getName());
+		 em.getTransaction().begin();
+		 SimpleEntity simple = new SimpleEntity();
+		 simple.setName("simple");
+		 em.persist(simple);
+		 em.getTransaction().commit();
+		 assertNotNull(simple.getId());
+		 SimpleEntity s = em.find(SimpleEntity.class, simple.getId());
+		 assertEquals("simple", s.getName());
 	}
 }
