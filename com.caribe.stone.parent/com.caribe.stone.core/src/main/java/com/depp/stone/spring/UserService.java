877521bca46e0f14c5937d08050d7d516929fe9c
yan-zhang <rockflyrain@gmail.com>
2012-02-03 16:55:49 +0800

    add new project <spring> move test to spring project.

--- com.caribe.stone.parent/com.caribe.stone.core/src/main/java/com/depp/stone/spring/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.depp.stone.spring;
-
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-public interface UserService {
-	boolean hasMatchUser(String userName, String password);
-}
--- com.caribe.stone.parent/com.caribe.stone.core/src/test/java/com/depp/stone/spring/bean/instantiation/ConstrtorBean.java
+++
@@ -1,9 +0,0 @@
-package com.depp.stone.spring.bean.instantiation;
-
-public class ConstrtorBean {
-	public ConstrtorBean(String str,int i,Bar bar) {
-	}
-}
-class Bar{
-	public Bar(){}
-}
