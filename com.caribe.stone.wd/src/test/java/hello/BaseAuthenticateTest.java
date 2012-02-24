787d47229aa6622ca9ceb9932131ad7819702933
yan-zhang <rockflyrain@gmail.com>
2012-02-24 17:18:23 +0800

    U

---
+++ com.caribe.stone.wd/src/test/java/hello/BaseAuthenticateTest.java
@@ -0,0 +1,25 @@
+package hello;
+
+import org.openqa.selenium.Cookie;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class BaseAuthenticateTest {
+
+	public static void main(String[] args) {
+		WebDriver wd;
+		AuthenticatedHtmlUnitDriver.setCredentials("tomcat", "tomcat");
+		DesiredCapabilities capabilities = new DesiredCapabilities();
+		wd = new HtmlUnitDriver(true);
+		Cookie cookie = new Cookie("Authorization", "Basic tomcat");
+		// var header = "Basic " + $.base64.encode(auth.username + ":" +
+		// auth.password);
+		// document.cookie = "Authorization: " + header;
+		wd.get("http://localhost:8090/");
+		JavascriptExecutor js = (JavascriptExecutor) wd;
+		System.out.println(js.executeScript("return document.title"));
+
+	}
+}
