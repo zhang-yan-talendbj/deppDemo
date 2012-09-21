bf7e0f7c222d1bb64243b0c5781bb0104ee6e521
thinkdeeply <rockflyrain@gmail.com>
2012-09-21 11:50:23 +0800

    fix sonar error.

---
+++ com.caribe.stone.core/src/test/java/com/caribe/stone/java/generics/GenericsDemo.java
@@ -0,0 +1,48 @@
+package com.caribe.stone.java.generics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class GenericsDemo {
+	public void inspect(List<Father> list) {
+		for (Object obj : list) {
+			System.out.println(obj);
+		}
+		Son son = null;
+		list.add(son); // 这个操作在当前方法的上下文是合法的。
+	}
+
+	void printCollection(Collection<?> c) {
+		for (Object e : c) {
+			System.out.println(e);
+		}
+	}
+
+	public void test() {
+		List<Object> strs = new ArrayList<Object>();
+		// inspect(strs); // 编译错误
+	}
+
+	public void wildcard(List<? extends Number> list) {
+		list.get(1).intValue();
+	}
+	
+	public static void main(String[] args) {
+		List<String> arrayList = new ArrayList<String>();
+		unsafeAdd(arrayList,new Integer(44));
+		String s = arrayList.get(0);
+	}
+
+	private static void unsafeAdd(List arrayList, Integer integer) {
+		arrayList.add(integer);
+	}
+}
+
+class Father {
+
+}
+
+class Son extends Father {
+
+}
--- com.caribe.stone.cucumber/src/test/java/org/com/caribe/stone/RunCukesTest.java
+++ com.caribe.stone.cucumber/src/test/java/org/com/caribe/stone/RunCukesTest.java
@@ -3,15 +3,14 @@ package org.com.caribe.stone;
 
 import org.junit.runner.RunWith;
 
-import cucumber.annotation.en.Given;
-import cucumber.annotation.en.Then;
-import cucumber.annotation.en.When;
 import cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(
-		features={"src/test/resources/multString.feature"},
-		format = {"pretty", "html:target/cucumber"})
+		features={"src/test/resources/multString.feature"}
+//		,format = "junit:target/aa"
+//		,format = {"pretty", "html:target/cucumber"}
+		)
 public class RunCukesTest {
 
 }
